cc686ab65d207900123867cf82b4b05e0e8d84a6
Keckster <dsk073@gmail.com>
2016-08-11 23:04:53 -0400

    Movement

---
+++ oh gee wiz/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = Input.GetAxis ("Mouse Y");
+
+		float z = transform.eulerAngles.z;
+		transform.Rotate (0, 0, -z);
+
+		if (mouseX < 0) {
+			Player.transform.Rotate (0, mouseX, 0);
+		}
+		else if (mouseX > 0) {
+			Player.transform.Rotate (0, mouseX, 0);
+		}
+		if (mouseY < 0) {
+			gameObject.transform.RotateAround (Player.transform.position, new Vector3 ( -mouseY, 0, 0), 1);
+		}
+		else if (mouseY > 0) {
+			gameObject.transform.RotateAround (Player.transform.position, new Vector3 ( -mouseY, 0, 0), 1);
+		}
+		//gameObject.transform.rotation = Quaternion.Euler (transform.rotation.x, transform.position.y, 0);
+	}
+}
---
+++ oh gee wiz/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+
+	private Rigidbody R;
+
+	void Start () {
+		
+		R = gameObject.GetComponent<Rigidbody> ();
+	
+	}
+
+
+	void Update () {
+
+
+
+	}
+
+
+	void FixedUpdate () {
+
+		float MoveH = Input.GetAxis ("Horizontal");
+		float MoveV = Input.GetAxis ("Vertical");
+
+		//if (MoveH > 0) {
+			//R.velocity = transform.right * Speed;
+		//}
+		//else if (MoveH < 0) {
+			//R.velocity = -transform.right * Speed;
+		//}
+		//if (MoveV > 0) {
+			//R.velocity = transform.forward * Speed;
+		//}
+		//else if (MoveV < 0) {
+			//R.velocity = -transform.forward * Speed;
+		//}
+		transform.position += transform.TransformDirection(MoveH * Speed, 0f, MoveV * Speed);
+
+	}
+}
